4d13ef169e13d3dfba4b8df300cb269086f346b9
Safiyya A <146670910+lanternofsea@users.noreply.github.com>
2023-10-27 17:48:19 -0400

    Add files via upload

---
+++ artists2.py
@@ -0,0 +1,6 @@
+
+fav_artists = ["Ado","Sleeping At Last","Two Door Cinema Club", "Lovejoy", "Araki"] #list of artists
+songs = ["I'm A Controversy","Mars","Undercover Martian","Call Me What You Like","Teo"] #list of songs
+
+for index, artist_name in enumerate(fav_artists): #iterate over the artists
+    print(f"Artist: {artist_name}, Favorite Song: {songs[index]}") #output
